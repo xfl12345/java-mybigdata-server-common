6efd8a15f729941c92bb8c882fa6ff7c226c6f87
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-12 13:08:37 +0800

    迁移 Web API 一些常量。

---
+++ src/main/java/cc/xfl12345/mybigdata/server/common/appconst/api/request/JsonApiRequestField.java
@@ -0,0 +1,14 @@
+package cc.xfl12345.mybigdata.server.common.appconst.api.request;
+
+public class JsonApiRequestField {
+
+    /**
+     * JSON API根路径上的操作
+     * 有点路由的味道
+     */
+    public static class Operation {
+        public static final String ACCOUNT = "account";
+        public static final String FILE = "file";
+    }
+
+}
---
+++ src/main/java/cc/xfl12345/mybigdata/server/common/appconst/api/request/LoginRequestField.java
@@ -0,0 +1,6 @@
+package cc.xfl12345.mybigdata.server.common.appconst.api.request;
+
+public class LoginRequestField {
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+}
