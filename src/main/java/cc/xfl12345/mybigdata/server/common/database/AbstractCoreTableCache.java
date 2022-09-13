c16a84447d8e52f776a09fc3d559b8e19ca47216
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-13 15:16:59 +0800

    CoreTableCache 里的 fieldCanNotBeNullMessageTemplate 统一使用 AppConst 常量。

--- src/main/java/cc/xfl12345/mybigdata/server/common/database/AbstractCoreTableCache.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/database/AbstractCoreTableCache.java
@@ -1,12 +1,18 @@
 package cc.xfl12345.mybigdata.server.common.database;
 
+import cc.xfl12345.mybigdata.server.common.appconst.AppConst;
 import cc.xfl12345.mybigdata.server.common.pojo.TwoWayMap;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 
 @Slf4j
 public abstract class AbstractCoreTableCache <IdType, ValueType> implements InitializingBean {
+    @Getter
+    @Setter
+    protected String fieldCanNotBeNullMessageTemplate = AppConst.FIELD_CAN_NOT_BE_NULL_MESSAGE_TEMPLATE;
+
     @Getter
     protected TwoWayMap<ValueType, IdType> tableNameCache;
 
