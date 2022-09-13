50e72cf51a5cb601ec1301f31afd930e8a395307
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-13 13:07:34 +0800

    新增 APP 自己的乱七八糟常量类 AppConst 。

---
+++ src/main/java/cc/xfl12345/mybigdata/server/common/appconst/AppConst.java
@@ -0,0 +1,5 @@
+package cc.xfl12345.mybigdata.server.common.appconst;
+
+public class AppConst {
+    public static final String FIELD_CAN_NOT_BE_NULL_MESSAGE_TEMPLATE = "Property [%s] can not be null!";
+}
