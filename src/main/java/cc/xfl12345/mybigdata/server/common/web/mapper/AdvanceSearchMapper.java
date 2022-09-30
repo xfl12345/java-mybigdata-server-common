bd20afa6b7884a6e65da3b5654f8ddd0bbebd61a
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-30 14:24:11 +0800

    重新定义 AdvanceSearchMapper 前缀搜索 API 返回值结构，绑上 ID，方便快速查询访问。

--- src/main/java/cc/xfl12345/mybigdata/server/common/web/mapper/AdvanceSearchMapper.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/web/mapper/AdvanceSearchMapper.java
@@ -1,16 +1,17 @@
 package cc.xfl12345.mybigdata.server.common.web.mapper;
 
 import cc.xfl12345.mybigdata.server.common.data.condition.SingleTableCondition;
+import cc.xfl12345.mybigdata.server.common.pojo.IdAndValue;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface AdvanceSearchMapper {
-    public List<String> selectStringByPrefix(String prefix);
+    public List<IdAndValue<String>> selectStringByPrefix(String prefix);
 
-    public List<BigDecimal> selectNumberByPrefix(String prefix);
+    public List<IdAndValue<BigDecimal>> selectNumberByPrefix(String prefix);
 
-    public List<BigDecimal> selectNumberByPrefix(Integer prefix);
+    public List<IdAndValue<BigDecimal>> selectNumberByPrefix(Integer prefix);
 
     public List<Object> selectByCondition(SingleTableCondition condition);
 }
