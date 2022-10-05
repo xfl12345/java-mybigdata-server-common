6cb8fe1b40e8541e448182029c50112b418c5309
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-05 14:19:41 +0800

    新增 默认APP名称 常量。

--- src/main/java/cc/xfl12345/mybigdata/server/common/appconst/AppConst.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/appconst/AppConst.java
@@ -2,4 +2,6 @@ package cc.xfl12345.mybigdata.server.common.appconst;
 
 public class AppConst {
     public static final String FIELD_CAN_NOT_BE_NULL_MESSAGE_TEMPLATE = "Property [%s] can not be null!";
+
+    public static final String DEFAULT_APP_NAME = "mybigdata";
 }
