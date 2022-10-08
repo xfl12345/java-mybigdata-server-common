7130965cdba857f0db694660f4970aeb6dee7b7c
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-08 11:02:24 +0800

    纠正名称。

--- src/main/java/cc/xfl12345/mybigdata/server/common/data/handler/GlobalDataRecordHandler.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/data/handler/GlobalDataRecordHandler.java
@@ -2,5 +2,5 @@ package cc.xfl12345.mybigdata.server.common.data.handler;
 
 import cc.xfl12345.mybigdata.server.common.pojo.CommonGlobalDataRecord;
 
-public class IdHandler extends SingleTableDataHandler<CommonGlobalDataRecord> {
+public class GlobalDataRecordHandler extends SingleTableDataHandler<CommonGlobalDataRecord> {
 }
