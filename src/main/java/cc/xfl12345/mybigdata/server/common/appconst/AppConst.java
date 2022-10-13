23bff22dcab1108c857685dda64ddc47cea1098f
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-13 20:46:55 +0800

    重命名 “Mapper 找不到” 错误信息模板。

--- src/main/java/cc/xfl12345/mybigdata/server/common/appconst/AppConst.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/appconst/AppConst.java
@@ -3,7 +3,7 @@ package cc.xfl12345.mybigdata.server.common.appconst;
 public class AppConst {
     public static final String FIELD_CAN_NOT_BE_NULL_MESSAGE_TEMPLATE = "Property [%s] can not be null!";
 
-    public static final String MAPPER_NOT_FOUND_MESSAGE_TEMPLATE = "Mapper [%s] missing.";
+    public static final String MAPPER_NOT_FOUND_MESSAGE_TEMPLATE = "The mapper of [%s] is missing.";
 
     public static final String DEFAULT_APP_NAME = "mybigdata";
 }
