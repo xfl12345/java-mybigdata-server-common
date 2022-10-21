3e3184d54ef95b52d6e6661918ef03d7f6d22bf6
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-21 10:32:54 +0800

    去除对 fastJSON 的依赖。修复一个潜在的空指针 BUG

--- src/main/java/cc/xfl12345/mybigdata/server/common/utility/ConsoleCharsetUtils.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/utility/ConsoleCharsetUtils.java
@@ -1,12 +1,14 @@
 package cc.xfl12345.mybigdata.server.common.utility;
 
-import com.alibaba.fastjson2.JSONReader;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jna.Platform;
 import com.sun.jna.platform.win32.Kernel32;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.PostConstruct;
 import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.IllegalCharsetNameException;
@@ -20,18 +22,14 @@ public class ConsoleCharsetUtils {
 
     protected LinkedHashMap<String, String> microsoftWindowsCodePageMap = new LinkedHashMap<>();
 
+    @SuppressWarnings("unchecked")
     @PostConstruct
     public void init() throws Exception {
         URL localMapFileURL = Thread.currentThread().getContextClassLoader().getResource(microsoftWindowsCodePageJsonResourcePath);
         if (localMapFileURL != null) {
-            JSONReader reader = JSONReader.of(
-                localMapFileURL.openConnection().getInputStream(),
-                StandardCharsets.UTF_8
-            );
-            reader.read(
-                microsoftWindowsCodePageMap,
-                0
-            );
+            Reader reader = new InputStreamReader(localMapFileURL.openStream(), StandardCharsets.UTF_8);
+            ObjectMapper mapper = new ObjectMapper();
+            microsoftWindowsCodePageMap = (LinkedHashMap<String, String>) mapper.readValue(reader, LinkedHashMap.class);
             reader.close();
         } else {
             throw new FileNotFoundException(microsoftWindowsCodePageJsonResourcePath);
@@ -54,10 +52,12 @@ public class ConsoleCharsetUtils {
             }
             case Platform.LINUX -> {
                 String currentLang = System.getenv("LANG");
-                int dotSplitCharacterIndex = currentLang.lastIndexOf('.');
-                if (dotSplitCharacterIndex > 0 && dotSplitCharacterIndex < currentLang.length() - 2) {
-                    String charsetName = currentLang.substring(dotSplitCharacterIndex + 1);
-                    charset = getCharsetForName(charsetName);
+                if (currentLang != null) {
+                    int dotSplitCharacterIndex = currentLang.lastIndexOf('.');
+                    if (dotSplitCharacterIndex > 0 && dotSplitCharacterIndex < currentLang.length() - 2) {
+                        String charsetName = currentLang.substring(dotSplitCharacterIndex + 1);
+                        charset = getCharsetForName(charsetName);
+                    }
                 }
             }
         }
