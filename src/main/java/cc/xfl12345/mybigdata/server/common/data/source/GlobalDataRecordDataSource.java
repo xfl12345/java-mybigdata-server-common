f7b9dea3da2c851fd9685f9f3acd829524c347c8
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-27 15:41:11 +0800

    IdDataSource 改名为 GlobalDataRecordDataSource

--- src/main/java/cc/xfl12345/mybigdata/server/common/data/source/GlobalDataRecordDataSource.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/data/source/GlobalDataRecordDataSource.java
@@ -2,5 +2,5 @@ package cc.xfl12345.mybigdata.server.common.data.source;
 
 import cc.xfl12345.mybigdata.server.common.database.pojo.CommonGlobalDataRecord;
 
-public interface IdDataSource extends DataSource<CommonGlobalDataRecord> {
+public interface GlobalDataRecordDataSource extends DataSource<CommonGlobalDataRecord> {
 }
