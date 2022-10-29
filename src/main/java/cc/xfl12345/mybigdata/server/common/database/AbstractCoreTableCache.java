5c5bfb8fa2bdde3bda415aafc3c77eb997a2f8fe
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-29 19:06:11 +0800

    改进 AbstractCoreTableCache 对 Boolean 类型的支持。

--- src/main/java/cc/xfl12345/mybigdata/server/common/database/AbstractCoreTableCache.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/database/AbstractCoreTableCache.java
@@ -1,6 +1,7 @@
 package cc.xfl12345.mybigdata.server.common.database;
 
 import cc.xfl12345.mybigdata.server.common.appconst.AppConst;
+import cc.xfl12345.mybigdata.server.common.database.error.TableDataException;
 import cc.xfl12345.mybigdata.server.common.pojo.TwoWayMap;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,4 +33,20 @@ public abstract class AbstractCoreTableCache <ID, Value> {
     public abstract void refreshBooleanCache() throws Exception;
 
     public abstract void refreshCoreTableNameCache() throws Exception;
+
+    protected abstract String tableNameOfBoolean();
+
+    public boolean getBooleanById(ID globalId) {
+        if (idOfTrue.equals(globalId)) {
+            return true;
+        }
+        if (idOfFalse.equals(globalId)) {
+            return false;
+        }
+        throw new TableDataException(
+            "The reference of id '" + globalId + "' is not a boolean value.",
+            new Object[]{ globalId },
+            tableNameOfBoolean()
+        );
+    }
 }
