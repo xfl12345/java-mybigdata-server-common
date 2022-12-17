c92824c2a91d5c74001fa36161d72a86a1c58595
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-12-17 21:22:26 +0800

    迁移 OpenCloneable 接口所在的 package 到 pojo ，添加 Spring 框架内部定义的工具类。

--- src/main/java/cc/xfl12345/mybigdata/server/common/pojo/OpenCloneable.java
+++ src/main/java/cc/xfl12345/mybigdata/server/common/pojo/OpenCloneable.java
@@ -1,4 +1,4 @@
-package cc.xfl12345.mybigdata.server.common.api;
+package cc.xfl12345.mybigdata.server.common.pojo;
 
 public interface OpenCloneable extends Cloneable {
     Object clone() throws CloneNotSupportedException;
